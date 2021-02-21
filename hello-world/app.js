5302dc93b128c84e85d643ecf33f5dce505560a2
sourabhDhariwal1234 <sdhariwal2@gmail.com>
2021-02-22 01:56:16 +0530

    insert new console log

--- hello-world/app.js
+++ hello-world/app.js
@@ -22,7 +22,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'Test With Ci Cd Pipeline',
             })
         }
     } catch (err) {
