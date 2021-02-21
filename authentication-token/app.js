09be1c6dd527eebf74f4f494a76760c703d621c7
sourabhDhariwal1234 <sdhariwal2@gmail.com>
2021-02-21 19:05:47 +0530

    initial commit

---
+++ authentication-token/app.js
@@ -0,0 +1,59 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+exports.lambdaHandler =  function(event, context, callback) {
+    var token = event.authorizationToken;
+    switch (token) {
+        case 'allow':
+            callback(null, generatePolicy('user', 'Allow', event.methodArn));
+            break;
+        case 'deny':
+            callback(null, generatePolicy('user', 'Deny', event.methodArn));
+            break;
+        case 'unauthorized':
+            callback("Unauthorized");   // Return a 401 Unauthorized response
+            break;
+        default:
+            callback("Error: Invalid token"); // Return a 500 Invalid token response
+    }
+};
+
+// Help function to generate an IAM policy
+var generatePolicy = function(principalId, effect, resource) {
+    var authResponse = {};
+    
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        var policyDocument = {};
+        policyDocument.Version = '2012-10-17'; 
+        policyDocument.Statement = [];
+        var statementOne = {};
+        statementOne.Action = 'execute-api:Invoke'; 
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+    
+    // Optional output with custom properties of the String, Number or Boolean type.
+    authResponse.context = {
+        "stringKey": "stringval",
+        "numberKey": 123,
+        "booleanKey": true
+    };
+    return authResponse;
+}
