f29decbb916ff53284e13a22bf79109f0e12e815
sourabhDhariwal1234 <sdhariwal2@gmail.com>
2021-02-22 12:42:58 +0530

    Add new lambda function NewTestFunction

---
+++ new-test-lambda/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.handler = (event, context, callback) => {
+    console.log(`Debug: event received: ${JSON.stringify(event)}`);
+    const body = `Hello ${event.body ? event.body : 'World'}!`;
+    const statusCode = 200;
+    // Just throw in some async stuff to mislead everybody <3 :) 
+    new Promise((resolve, reject) => resolve())
+        .then(_ => callback(null, { statusCode, body }))
+        .catch(error => callback(error));
+};
