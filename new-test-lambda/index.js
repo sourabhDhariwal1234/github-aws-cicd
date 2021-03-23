d3875343ce2475dba01ca1ce85941e2fd2b61b4d
sourabhDhariwal1234 <sdhariwal2@gmail.com>
2021-03-23 13:19:06 +0530

    Add console.log Function

--- new-test-lambda/index.js
+++ new-test-lambda/index.js
@@ -3,6 +3,7 @@ exports.handler = (event, context, callback) => {
     console.log(`Debug: event received: ${JSON.stringify(event)}`);
     const body = `Hello ${event.body ? event.body : 'World'}!`;
     const statusCode = 200;
+    console.log("Sourabh Dhariwal...")
     // Just throw in some async stuff to mislead everybody <3 :) 
     new Promise((resolve, reject) => resolve())
         .then(_ => callback(null, { statusCode, body }))
